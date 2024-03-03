2d32da8743a86fdc02d796c8ae795b2bcb2b3352
aziza <152301691+azizabaktybaeva@users.noreply.github.com>
2024-03-03 18:52:09 +0600

    Initial Commit

---
+++ app lab4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  app lab4
+//
+//  Created by Baktybayeva Aziza on 03.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
